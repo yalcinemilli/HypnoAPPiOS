1f49761b3eb7abd32c00ab8a39f05901f755757b
Yalcin Emilli <97788204+yalcinemilli@users.noreply.github.com>
2022-03-14 14:46:12 +0100

    Add files via upload

---
+++ HypnoPlussTests/HypnoPlussTests.swift
@@ -0,0 +1,33 @@
+//
+//  HypnoPlussTests.swift
+//  HypnoPlussTests
+//
+//  Created by Yalcin Emilli on 19.08.20.
+//
+
+import XCTest
+@testable import HypnoPluss
+
+class HypnoPlussTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
